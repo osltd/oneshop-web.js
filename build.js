1445e55c1e206457c15a30d9ac42bb3bef34dc2f
Lawson Cheng <inbox@lawsoncheng.com>
2019-12-19 18:10:46 +0800

    Updated build script, will sync to remote after release build

--- build.js
+++ build.js
@@ -3,7 +3,37 @@ let rm            = require('rimraf');
 let path          = require('path');
 let webpack       = require('webpack');
 let util          = require('util');
+let AWS           = require('aws-sdk');
+let fs            = require('fs');
 
+// load .env
+require('dotenv').config();
+
+AWS.config.update({
+    accessKeyId     : process.env.AWS_ACCESS_ID,
+    secretAccessKey : process.env.AWS_ACCESS_KEY,
+    region          : process.env.AWS_REGION
+});
+
+// push build to s3
+const pushToS3 = () => {
+    // read build
+    fs.readFile(`${__dirname}/dist/oneshop-sdk.min.js`, function (err, data) {
+        // error on reading file
+        if (err) throw err;
+        // upload to S3
+        new AWS.S3()
+        .putObject({
+            Bucket: process.env.AWS_BUCKET,
+            Key: 'oneshop-sdk.min.js',
+            Body: data
+        }, function (err, data) {
+            console.log(err ? err : 'Successfully uploaded sdk to remote.');
+        });
+    });
+};
+
+// compile callback
 const compileCallback = (er, stats) => {
     if (er) throw er
     stats = util.isArray(stats.stats) ? stats.stats : [stats]
@@ -18,11 +48,14 @@ const compileCallback = (er, stats) => {
         }) + '\n\n')
     });
     console.log('Build complete.\n');
+    pushToS3();
 };
 
+// remove old src
 rm(path.resolve(__dirname, './dist'), err => {
     if (err) throw err
     let compiler = webpack(webpackConfig)
-    
+    // compile
     compiler.run(compileCallback)
 });
+
