ea909b204ade5f5264f38abfef5be0f88d9b3630
Lawson Cheng <inbox@lawsoncheng.com>
2020-10-13 01:06:17 +0800

    added merchandise  module

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import OS from "./src/helpers/os_module";
 import Article from "./src/modules/article";
 import Cart from "./src/modules/cart";
 import Consumer from "./src/modules/consumer";
+import Merchandise from "./src/modules/merchandise";
 
 export default class Oneshop extends OS{
 
-    baseUrl? : string;
-    article  : Article;
-    cart     : Cart;
-    consumer : Consumer;
+    baseUrl?    : string;
+    article     : Article;
+    cart        : Cart;
+    consumer    : Consumer;
+    merchandise : Merchandise;
 
     constructor(baseUrl:string){
         super(baseUrl)
@@ -20,6 +22,7 @@ export default class Oneshop extends OS{
         this.article = new Article(this.getShopDomain());
         this.cart = new Cart(this.getShopDomain());
         this.consumer = new Consumer(this.getShopDomain());
+        this.merchandise = new Merchandise(this.getShopDomain());
     }
 
     setShopBaseURL(baseUrl:string){
