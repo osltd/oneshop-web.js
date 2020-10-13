170d6f8e2c5f5ee04dd625d1351286ea6227bf35
Lawson Cheng <inbox@lawsoncheng.com>
2020-10-13 10:41:38 +0800

    Use Shared interface

--- src/modules/order.ts
+++ src/modules/order.ts
@@ -1,33 +1,10 @@
 import { get, create } from '../helpers/request';
 import OS from '../helpers/os_module';
+import { OrderInfo } from '../helpers/interfaces';
 
 interface OrderHistory {
     get:Function;
 }
-
-interface OrderInfo {
-    items:string;
-    coupons?:string;
-    notes?:string;
-    shippings?:any;
-    shipping?:{
-        address:string;
-        country:string;
-    };
-    contact:{
-        email?:string;
-        phone?:string;
-        first_name:string;
-        last_name:string;
-    };
-    payment:{
-        source?:string;
-        card?:string;
-        exp_date?:string
-        csc?:Number;
-    };
-}
-
 
 export default class Order extends OS {
     
